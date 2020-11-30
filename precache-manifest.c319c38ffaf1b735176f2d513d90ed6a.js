17cf1c7b25461310ab9c2f56fb48bee2dfb183cd
Max <maxim.nebela@gmail.com>
2020-11-30 16:15:55 +0200

    Updates

---
+++ precache-manifest.c319c38ffaf1b735176f2d513d90ed6a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b360ad627b0a3bca35724498d495625",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "9a00d75f4f9447fa5a01",
+    "url": "/social-network/static/css/3.44693991.chunk.css"
+  },
+  {
+    "revision": "0d8591bddee0c877c400",
+    "url": "/social-network/static/css/4.5af23981.chunk.css"
+  },
+  {
+    "revision": "8c36ea9432bec4aed606",
+    "url": "/social-network/static/css/6.b5f2e7ea.chunk.css"
+  },
+  {
+    "revision": "154e0e6602aee79161d0",
+    "url": "/social-network/static/css/main.e128a5c5.chunk.css"
+  },
+  {
+    "revision": "bbf4512eaf5956e7c978",
+    "url": "/social-network/static/js/2.45ee8661.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social-network/static/js/2.45ee8661.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a00d75f4f9447fa5a01",
+    "url": "/social-network/static/js/3.62dcade3.chunk.js"
+  },
+  {
+    "revision": "0d8591bddee0c877c400",
+    "url": "/social-network/static/js/4.c8de6c35.chunk.js"
+  },
+  {
+    "revision": "de244353042819bb0db6",
+    "url": "/social-network/static/js/5.13f65633.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/social-network/static/js/5.13f65633.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c36ea9432bec4aed606",
+    "url": "/social-network/static/js/6.ef4650cc.chunk.js"
+  },
+  {
+    "revision": "154e0e6602aee79161d0",
+    "url": "/social-network/static/js/main.63b68cf5.chunk.js"
+  },
+  {
+    "revision": "4c9212c67db3826175b2",
+    "url": "/social-network/static/js/runtime-main.a483fc90.js"
+  },
+  {
+    "revision": "dc8b2f5ff390b397dff80ae612c41337",
+    "url": "/social-network/static/media/loading.dc8b2f5f.svg"
+  },
+  {
+    "revision": "7cc7a630624d20f7797cb4c8e93c09c1",
+    "url": "/social-network/static/media/user.7cc7a630.png"
+  }
+]);
