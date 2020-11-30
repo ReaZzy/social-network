8658a4e38738408bc28bf9638ea91d407a390e93
Max <maxim.nebela@gmail.com>
2020-11-30 16:38:08 +0200

    DEploy

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends React.Component{
   render() {
 
     return (
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
           <div>
             <HeaderContainer />
             <div className="app-wrapper">
